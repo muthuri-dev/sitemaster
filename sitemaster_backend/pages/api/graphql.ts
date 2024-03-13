a61f9df7604f0907c80a2d17e18f35c21e5f4eac
muthuri-dev <muthurikennedy082@gmail.com>
2024-03-13 10:32:57 +0300

    chores(CORS):Add cross-origin to be accessed and set introspection to true

--- sitemaster_backend/pages/api/graphql.ts
+++ sitemaster_backend/pages/api/graphql.ts
@@ -7,15 +7,18 @@ import { ApolloServer } from "@apollo/server";
 import { NextApiRequest, NextApiResponse } from "next";
 import { TContext } from "@/types";
 import prisma from "@/utils/db";
+import allowCors from "@/utils/cors";
 
 const apolloServer = new ApolloServer<TContext>({
   typeDefs,
   resolvers,
+  introspection: true,
 });
-export default startServerAndCreateNextHandler(apolloServer, {
+const handler = startServerAndCreateNextHandler(apolloServer, {
   context: async (req: NextApiRequest, res: NextApiResponse) => ({
     req,
     res,
     prisma,
   }),
 });
+export default allowCors(handler);
